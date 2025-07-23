f67cd4096e2c55379f486aa5da81cca3541b16d7
Sandra Montero <sms125@alu.ua.es>
2025-07-23 17:10:19 +0200

    Fixed an error in contact

--- src/app/components/Contact/Contact.tsx
+++ src/app/components/Contact/Contact.tsx
@@ -83,13 +83,20 @@ const Contact = () => {
 };
 
 export default Contact;
-
-const ContactTextArea = ({ row, placeholder, name, defaultValue }) => {
-  return (
+type ContactTextAreaProps = {
+    row?: string;
+    placeholder?: string;
+    name: string;
+    defaultValue?: string;
+    type?:string;
+  };
+  
+  const ContactTextArea = ({ row, placeholder, name, defaultValue }: ContactTextAreaProps) => {
+   return (
     <>
       <div className="mb-6">
         <textarea
-          rows={row}
+          rows={Number(row)}
           placeholder={placeholder}
           name={name}
           className="w-full resize-none rounded border border-stroke px-[14px] py-3 text-base text-body-color outline-none focus:border-primary dark:border-dark-3 dark:bg-dark dark:text-dark-6"
@@ -100,7 +107,7 @@ const ContactTextArea = ({ row, placeholder, name, defaultValue }) => {
   );
 };
 
-const ContactInputBox = ({ type, placeholder, name }) => {
+const ContactInputBox = ({ type, placeholder, name }: ContactTextAreaProps) => {
   return (
     <>
       <div className="mb-6">
