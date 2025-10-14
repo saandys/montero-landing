34274fdbf8c8bb61ca473280b9df0ce75e8931e4
Sandra Montero <sms125@alu.ua.es>
2025-10-14 23:23:54 +0200

    Fixed bug in npm run build

--- src/components/ui/floating-dock.tsx
+++ src/components/ui/floating-dock.tsx
@@ -65,35 +65,7 @@ const socialItems = [
       
 <div style={{ height: '100vh', background: '#1a1a1a' }}>
 
-  <StaggeredMenu
-
-    position="right"
-
-    items={menuItems}
-
-    socialItems={socialItems}
-
-    displaySocials={true}
-
-    displayItemNumbering={true}
-
-    menuButtonColor="#fff"
-
-    openMenuButtonColor="#fff"
-
-    changeMenuColorOnOpen={true}
-
-    colors={['#B19EEF', '#5227FF']}
-
-    logoUrl="/path-to-your-logo.svg"
-
-    accentColor="#ff6b6b"
-
-    onMenuOpen={() => console.log('Menu opened')}
-
-    onMenuClose={() => console.log('Menu closed')}
-
-  />
+ 
 
 </div>
     </div>
